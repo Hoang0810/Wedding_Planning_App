cc2e252352b9ab9b5d5f364a9473feed237ed3b3
latoanthinh <140524248+latoanthinh@users.noreply.github.com>
2025-02-08 22:27:33 +0700

    done Login UI

---
+++ src/Screens/SignIn.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import SignInPageStyles from "../Styles/SignInPageStyles";
+
+function SignIn_Page() {
+    return (
+        <View style={SignInPageStyles.container}>
+            <Text style={SignInPageStyles.welcomeText}>Welcome back!</Text>
+            <Text style={SignInPageStyles.instructionText}>Please, sign in to continue.</Text>
+
+            <TextInput
+                style={SignInPageStyles.input}
+                placeholder="Username"
+                placeholderTextColor="#aaa"
+            />
+            <TextInput
+                style={SignInPageStyles.input}
+                placeholder="Password"
+                placeholderTextColor="#aaa"
+                secureTextEntry
+            />
+
+            <View style={SignInPageStyles.rememberMeContainer}>
+                <View style={{ flexDirection: "row", }}>
+                    <TouchableOpacity>
+                        <Image source={require('../Assets/Images/checkbox.png')} style={SignInPageStyles.checkbox} />
+                    </TouchableOpacity>
+                    <Text style={SignInPageStyles.rememberMeText}>Remember me</Text>
+                </View>
+                <Text style={SignInPageStyles.forgotPasswordText}>Forgot password?</Text>
+
+            </View>
+
+            <TouchableOpacity style={SignInPageStyles.signInButton}>
+                <Text style={SignInPageStyles.signInButtonText}>Sign In</Text>
+            </TouchableOpacity>
+
+            <Text style={SignInPageStyles.orText}>Or sign in with</Text>
+            <View style={SignInPageStyles.socialButtonsContainer}>
+                <TouchableOpacity style={SignInPageStyles.socialButton}>
+                    <Image source={require('../Assets/Images/gg_btn.png')} style={SignInPageStyles.socialIcon} />
+                </TouchableOpacity>
+                <TouchableOpacity style={SignInPageStyles.socialButton}>
+                    <Image source={require('../Assets/Images/apple_btn.png')} style={SignInPageStyles.socialIcon} />
+                </TouchableOpacity>
+                <TouchableOpacity style={SignInPageStyles.socialButton}>
+                    <Image source={require('../Assets/Images/fb_btn.png')} style={SignInPageStyles.socialIcon} />
+                </TouchableOpacity>
+            </View>
+            <Text style={SignInPageStyles.guestText}>Continue as a Guest</Text>
+            <Text style={SignInPageStyles.signUpPrompt}>
+                Don't have an account? <Text style={SignInPageStyles.signUpText}>Sign up</Text>
+            </Text>
+        </View>
+    );
+}
+
+export default SignIn_Page;
---
+++ src/Styles/SignInPageStyles.js
@@ -0,0 +1,107 @@
+import { StyleSheet } from "react-native";
+
+const SignInPageStyles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 'auto',
+        justifyContent: 'center',
+        padding: 20,
+        backgroundColor: '#fff',
+    },
+    welcomeText: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 10,
+        color: '#333',
+        fontFamily: 'Playfair_me',
+    },
+    instructionText: {
+        fontSize: 16,
+        textAlign: 'center',
+        marginBottom: 20,
+        color: '#666',
+        fontFamily: 'Playfair_Me', 
+    },
+    input: {
+        height: 50,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        borderRadius: 8,
+        paddingHorizontal: 15,
+        marginBottom: 15,
+        backgroundColor: '#f9f9f9',
+    },
+    rememberMeContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    checkbox: {
+        width: 20,
+        height: 20,
+        marginRight: 10,
+    },
+    rememberMeText: {
+        color: '#000',
+        fontSize: 14,
+    },
+    forgotPasswordText: {
+        color: '#000',
+        fontSize: 14,
+    },
+    signInButton: {
+        backgroundColor: '#000',
+        paddingVertical: 15,
+        borderRadius: 5,
+        marginBottom: 20,
+    },
+    signInButtonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 16,
+        fontFamily: 'Playfair_me',
+    },
+    orText: {
+        textAlign: 'center',
+        marginVertical: 10,
+        color: '#000000',
+        fontSize: 17,
+    },
+    socialButtonsContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 20,
+    },
+    socialButton: {
+        width: 58,
+        height: 44,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    socialIcon: {
+        width: '100%',
+        height: '100%',
+    },
+    guestText: {
+        textAlign: 'start',
+        color: '#BABABA',
+        marginBottom: 10,
+        textDecorationLine: 'underline',
+        fontSize: 17,
+    },
+    signUpPrompt: {
+        textAlign: 'start',
+        color: '#000000',
+        fontSize: 17,
+    },
+    signUpText: {
+        color: '#BABABA',
+        fontSize: 17,
+        fontFamily: 'Playfair_me',
+    },
+});
+
+export default SignInPageStyles;
